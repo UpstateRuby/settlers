c4a95585eb3acc66fad0669965c0414ea6a6d5dd
Pete Broderick <simplybinary@gmail.com>
2015-02-11 20:00:43 -0500

    We now have a passing test suite

--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class GameTest < ActiveSupport::TestCase
   test "validates presence of name" do
     game = Game.new
-    game.save
+    game.valid?
     assert game.errors.messages[:name].any?
   end
 end
