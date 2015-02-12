c725ede12effb8d871521151ec6484da1bac4e41
Andrew Lechowicz <andrew@alecho.com>
2015-02-11 23:06:12 -0500

    Adds method to generate numbers for resource hexes

--- app/models/game.rb
+++ app/models/game.rb
@@ -10,7 +10,7 @@ class Game < ActiveRecord::Base
 
   # upon game creation cards_games, hexes (others?) will be created
 
-  
+
   def setup
     #assign theme if custom theme hasn't been selected
     assign_default_theme unless self.theme
@@ -22,13 +22,17 @@ class Game < ActiveRecord::Base
   end
 
   def create_hexes
-    numbers = [2,3,3,4,4,5,5,6,6,8,8,9,9,10,10,11,11,12]
+    # Original game is 18 + 1 dessert
+    # Expansion adds 10 + 1 dessert
+    # expansion adds 1 to every number
+    # expansion_numbers = [2,2,3,3,3,4,4,4,5,5,5,6,6,6,8,8,8,9,9,9,10,10,10,11,11,11,12,12]
+    numbers = self.get_numbers_for_resources
     resources = []
-    
+
     4.times { resources << 'lumber' }
     4.times { resources << 'wool' }
     4.times { resources << 'grain' }
-    3.times { resources << 'brick' }    
+    3.times { resources << 'brick' }
     3.times { resources << 'ore' }
 
     resources.shuffle
@@ -40,6 +44,30 @@ class Game < ActiveRecord::Base
     #desert.
     self.hexes.build(number: nil, resource: self.theme.desert_name)
 
+  end
+
+  def get_numbers_for_resources(total_resources = 18)
+    base_numbers = [2,3,3,4,4,5,5,6,6,8,8,9,9,10,10,11,11,12]
+    numbers = base_numbers
+    expansion = [2,3,4,5,6,8,9,10,11,12]
+
+    if total_resources < 19
+      # Nedd at least 18 hexes to play
+      return numbers
+    end
+
+    resources_above_base = total_resources - base_numbers.count
+    expansion_count = resources_above_base / expansion.count
+    expansion_count.times do
+      numbers += expansion
+    end
+
+    if (resources_above_base % expansion.count) == 0
+      return numbers
+    end
+
+    numbers += expansion.sample(resources_above_base % expansion.count)
+    return numbers
   end
 
 end
--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -12,4 +12,56 @@ class GameTest < ActiveSupport::TestCase
     game.save
     assert(game.hexes.count == 19)
   end
+
+  test "getting numbers for resources" do
+    game = Game.new
+    numbers = game.get_numbers_for_resources
+    assert(numbers == [2,3,3,4,4,5,5,6,6,8,8,9,9,10,10,11,11,12])
+  end
+
+  test "getting numbers for resources with less than default" do
+    game = Game.new
+    numbers = game.get_numbers_for_resources(1)
+    assert(numbers == [2,3,3,4,4,5,5,6,6,8,8,9,9,10,10,11,11,12])
+  end
+
+  test "getting numbers for resources with 1 more than default" do
+    game = Game.new
+    numbers = game.get_numbers_for_resources(19)
+    assert(numbers.count == 19, "Did not return correct amount of numbers for resources")
+  end
+
+  test "getting numbers for resources with 5 more than default" do
+    game = Game.new
+    numbers = game.get_numbers_for_resources(23)
+    assert(numbers.count == 23, "Did not return correct amount of numbers for resourcs")
+  end
+
+  test "getting numbers for resources with 9 more than default" do
+    game = Game.new
+    numbers = game.get_numbers_for_resources(27).sort
+    assert(numbers.count == 27, "Did not return correct amount of numbers for resources")
+  end
+
+  test "getting numbers for resources with standard expansion" do
+    game = Game.new
+    numbers = game.get_numbers_for_resources(28).sort
+    assert(numbers == [2,2,3,3,3,4,4,4,5,5,5,6,6,6,8,8,8,9,9,9,10,10,10,11,11,11,12,12])
+  end
+
+  test "getting numbers for resources with multiple (3) of expansions" do
+    game = Game.new
+    numbers = game.get_numbers_for_resources(48)
+    assert(
+      numbers ==
+      [
+        2,3,3,4,4,5,5,6,6,8,8,9,9,10,10,11,11,12,
+        2,3,4,5,6,8,9,10,11,12,
+        2,3,4,5,6,8,9,10,11,12,
+        2,3,4,5,6,8,9,10,11,12
+      ],
+      "Failed to generate expected array with a multiple of expansions"
+    )
+  end
+
 end
