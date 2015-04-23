4b7a2374377c3ad33f608ae1150692c65e8086ee
Pete Broderick <simplybinary@gmail.com>
2015-04-22 20:21:08 -0400

    WIP

--- public/libs/board.js
+++ public/libs/board.js
@@ -70,6 +70,17 @@ require(['underscore', 'KineticJS'], function(_, Kinetic){
         return group;
     };
 
+        var imageObj = new Image();
+          imageObj.onload = function() {
+            var canvas= document.getElementsByTagName('canvas')[0];
+            var ctx=canvas.getContext("2d");
+            var pattern = ctx.createPattern(imageObj, 'repeat');
+            ctx.rect(0, 0, canvas.width, canvas.height);
+            ctx.fillStyle = pattern;
+            ctx.fill();
+          };
+        imageObj.src = 'http://www.html5canvastutorials.com/demos/assets/wood-pattern.png';
+
     var boardSize = (game.hexes.length-1)/6;
     layer.add(hexagonalBloom(boardSize));
     layer.draw();
